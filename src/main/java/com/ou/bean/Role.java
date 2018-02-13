eb99576956a9fa432f4296fb99215828d00af7db
L_underscore <1696229469@qq.com>
2018-02-13 11:26:26 +0800

    mybatis generator 生成bean dao mapper

---
+++ src/main/java/com/ou/bean/Role.java
@@ -0,0 +1,35 @@
+package com.ou.bean;
+
+public class Role {
+    private Integer rid;
+
+    private String role;
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role == null ? null : role.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", rid=").append(rid);
+        sb.append(", role=").append(role);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/ou/dao/UserRoleMapper.java
@@ -0,0 +1,30 @@
+package com.ou.dao;
+
+import com.ou.bean.UserRole;
+import com.ou.bean.UserRoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserRoleMapper {
+    long countByExample(UserRoleExample example);
+
+    int deleteByExample(UserRoleExample example);
+
+    int deleteByPrimaryKey(Integer urid);
+
+    int insert(UserRole record);
+
+    int insertSelective(UserRole record);
+
+    List<UserRole> selectByExample(UserRoleExample example);
+
+    UserRole selectByPrimaryKey(Integer urid);
+
+    int updateByExampleSelective(@Param("record") UserRole record, @Param("example") UserRoleExample example);
+
+    int updateByExample(@Param("record") UserRole record, @Param("example") UserRoleExample example);
+
+    int updateByPrimaryKeySelective(UserRole record);
+
+    int updateByPrimaryKey(UserRole record);
+}
