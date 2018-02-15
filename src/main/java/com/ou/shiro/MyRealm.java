71aa4bfd54f395bbe9951a6b2041805e27045c70
L_underscore <1696229469@qq.com>
2018-02-15 11:33:30 +0800

    配置shiro realm

---
+++ src/main/java/com/ou/shiro/MyRealm.java
@@ -0,0 +1,51 @@
+package com.ou.shiro;
+
+import com.ou.bean.User;
+import com.ou.service.UserService;
+import com.ou.util.StringUtil;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MyRealm extends AuthorizingRealm {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public String getName() {
+        return "myRealm";
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String) token.getPrincipal();
+        System.out.println("22222222");
+        // 没有输入用户名 或则空用户名则返回空
+        if (StringUtil.checkNullOrEmpty(username)) {
+            return null;
+        }
+        User user = userService.getUserByUsername(username);
+        // 没有此用户名则返回空
+        if (null == user) {
+            return null;
+        }
+        String password = user.getPassword();
+        String salt = username;
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(username,
+                password, ByteSource.Util.bytes(salt), getName());
+        System.out.println("helloworld");
+        return authenticationInfo;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+}
