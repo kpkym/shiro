b77bb6d66b7cfaf1d00b0e425ff37acfa8eec4b2
L_underscore <1696229469@qq.com>
2018-02-16 13:44:18 +0800

    添加：插入用户权限功能

--- src/main/java/com/ou/service/impl/UserPermissionServiceImpl.java
+++ src/main/java/com/ou/service/impl/UserPermissionServiceImpl.java
@@ -1,33 +1,53 @@
 package com.ou.service.impl;
 
+import com.ou.bean.Permission;
 import com.ou.bean.User;
+import com.ou.bean.UserPermission;
 import com.ou.dao.UserPermissionMapper;
+import com.ou.service.PermissionService;
 import com.ou.service.UserPermissionService;
 import com.ou.service.UserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author: kpkym
  * date: 2018/2/15
  * time: 16:06
  */
+@Service
 public class UserPermissionServiceImpl implements UserPermissionService {
 
     @Autowired
     UserPermissionMapper userPermissionMapper;
     @Autowired
     UserService userService;
+    @Autowired
+    PermissionService permissionService;
 
     @Override
     public int insertUserPermission(String permission) {
         Subject subject = SecurityUtils.getSubject();
         String principal = (String)subject.getPrincipal();
         User user = userService.getUserByUsername(principal);
-        Integer uid = user.getUid();
-        // TODO 得到permission后插入userpermission
+        Permission perm = permissionService.getPermission(permission);
 
-        return 0;
+        Integer uid = user.getUid();
+        Integer pid = perm.getPid();
+        Calendar calendar = Calendar.getInstance();
+        // 过期时间在当前时间上增加100秒
+        calendar.add(Calendar.SECOND, 100);
+        Date expireTime = calendar.getTime();
+
+        UserPermission userPermission = new UserPermission();
+        userPermission.setUid(uid);
+        userPermission.setPid(pid);
+        userPermission.setExpireTime(expireTime);
+        return userPermissionMapper.insertSelective(userPermission);
     }
 }
