fdb0aed0dc5c3b2adc6406e83ca3ee298a18a851
L_underscore <1696229469@qq.com>
2018-02-16 13:28:03 +0800

    创建PermissionService

--- src/main/java/com/ou/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/ou/service/impl/PermissionServiceImpl.java
@@ -5,6 +5,7 @@ import com.ou.bean.PermissionExample;
 import com.ou.dao.PermissionMapper;
 import com.ou.service.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
  * date: 2018/2/16
  * time: 13:12
  */
+@Service
 public class PermissionServiceImpl implements PermissionService {
     @Autowired
     PermissionMapper permissionMapper;
