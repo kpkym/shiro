0c30c7d3175554cdfb05c9cc5efe227dc18c2ff5
L_underscore <1696229469@qq.com>
2018-02-20 21:25:23 +0800

    优化代码

--- src/main/java/com/ou/util/StringUtil.java
+++ src/main/java/com/ou/util/StringUtil.java
@@ -7,9 +7,6 @@ public class StringUtil {
      * @return 如果为空或为空字符串则返回true 否则返回false
      */
     public static boolean checkNullOrEmpty(String string) {
-        if (null == string || "".equals(string)) {
-            return true;
-        }
-        return false;
+        return null == string || "".equals(string);
     }
 }
