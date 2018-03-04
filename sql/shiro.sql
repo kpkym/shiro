59d9447260454ce935cdc1362870d89cc5c5b43c
L_underscore <1696229469@qq.com>
2018-03-04 12:16:34 +0800

    sql

---
+++ sql/shiro.sql
@@ -0,0 +1,59 @@
+﻿# Host: localhost  (Version: 5.7.3-m13)
+# Date: 2018-03-04 12:15:13
+# Generator: MySQL-Front 5.3  (Build 4.269)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "permission"
+#
+
+CREATE TABLE `permission` (
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `permission` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`pid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "role"
+#
+
+CREATE TABLE `role` (
+  `rid` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`rid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "role_permission"
+#
+
+CREATE TABLE `role_permission` (
+  `rpid` int(11) NOT NULL AUTO_INCREMENT,
+  `rid` int(11) NOT NULL DEFAULT '0',
+  `pid` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`rpid`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "user"
+#
+
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "user_role"
+#
+
+CREATE TABLE `user_role` (
+  `urid` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL DEFAULT '0',
+  `rid` int(11) NOT NULL DEFAULT '0',
+  `expire_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`urid`)
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
